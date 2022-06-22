5e67ce332ff776ad2fac803ec9a20ab0a0461d86
JairoDuenas <jairoduenas.ing@gmail.com>
2022-06-22 14:44:45 -0500

    Estilos globales con styled-components

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,9 +8,11 @@
 
 const React = require("react")
 const Layout = require("./src/components/layout").default
+const { GlobalStyles } = require("./src/styles")
 
-exports.wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
-  return <Layout {...props}>{element}</Layout>
-}
+exports.wrapRootElement = ({ element }) => (
+  <>
+    <GlobalStyles />
+    <Layout>{element}</Layout>
+  </>
+)
